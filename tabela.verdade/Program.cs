246370f978d8338432d48e5fa2415986c6ee167a
Lucivaldo Santos <lucivaldocarneiro18@gmail.com>
2019-06-16 13:48:11 -0300

    primeiro commit

---
+++ tabela.verdade/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace tabela.verdade
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Vamos construir a tabela verdade do E e do OU com suas afirmativas.");
+            Console.WriteLine("Insira a primeira afirmação:");
+            string pergunta1 = Console.ReadLine();
+            Console.WriteLine("Insira a segunda afirmação");
+            string pergunta2 = Console.ReadLine();
+
+            Console.WriteLine("A frase '" + pergunta1 + "' é verdadeira?");
+            Console.WriteLine("Responda com True ou False:");
+            string resposta1 = Console.ReadLine();
+
+            Console.WriteLine("A frase '" + pergunta2 + "' é verdadeira?");
+            Console.WriteLine("Responda com True ou False:");
+            string resposta2 = Console.ReadLine();
+
+            if (resposta1.Contains("True") && resposta2.Contains("True"))
+            {
+                Console.WriteLine("O resutada lógico da frase: " + pergunta1 + " e " + pergunta2 + " é: True");
+            }
+            else
+            {
+                Console.WriteLine("O resutada lógico da frase: " + pergunta1 + " e " + pergunta2 + " é: False");
+            }
+            if (resposta1.Contains("True") || resposta2.Contains("True"))
+            {
+                Console.WriteLine("O resutada lógico da frase: " + pergunta1 + " ou " + pergunta2 + " é: True");
+            }
+            else
+            {
+                Console.WriteLine("O resutada lógico da frase: " + pergunta1 + " ou " + pergunta2 + " é: False");
+            }
+        }
+    }
+}
+
